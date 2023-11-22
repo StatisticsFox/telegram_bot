e986f06eb0dda50ae4f0053a1c2fa8ba6c37586d
StatisticsFox <akfktl328@naver.com>
2023-11-22 23:46:52 +0900

    동기 비동기 수정

--- one_minutes.py
+++ one_minutes.py
@@ -1,11 +1,28 @@
+import telegram
 import schedule
 import time
+import datetime
+import pytz
+
+token = "6801716374:AAHf9y6oCdGKPLRvXG3vHvNNgVSIoZxhPWA"
+public_chat_name = '@OS_kgu'
+
+def send_message():
+    bot = telegram.Bot(token=token)
+    bot.sendMessage(chat_id=public_chat_name, text="alarm arrived!!!!!")
+
 def job():
-    now = time.localtime()
-    print("current time = ", str(now))
+    now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+    if 6 <= now.hour < 23:
+        send_message()
 
-schedule.every(1).minutes.do(job)
+# Schedule the job to run every 30 minutes
+schedule.every(30).minutes.do(job)
 
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+def main():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
